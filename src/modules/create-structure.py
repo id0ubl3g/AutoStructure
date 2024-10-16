543e81646d70dc21bbc4a6cd571dae7fef778c38
George Victor <id0ubl3g@gmail.com>
2024-10-15 21:43:39 -0300

    chore(create-structure): declare initial variables

---
+++ src/modules/create-structure.py
@@ -0,0 +1,7 @@
+class CreateStructure:
+    def __init__(self):
+        self.root_directory: str = "projects"
+        self.subdirectories = []
+        self.directory_not_exists: bool = None
+
+    
