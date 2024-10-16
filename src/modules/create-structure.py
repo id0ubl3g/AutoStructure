bebd4578cec108ee33c477337f65456c27d82e77
George Victor <id0ubl3g@gmail.com>
2024-10-15 21:51:29 -0300

    feat: add functions to get current directory and concatenate paths

--- src/modules/create-structure.py
+++ src/modules/create-structure.py
@@ -1,7 +1,18 @@
+import os
+
 class CreateStructure:
     def __init__(self):
         self.root_directory: str = "projects"
         self.subdirectories = []
         self.directory_not_exists: bool = None
 
+    def get_current_directory(self):
+        return os.getcwd()
     
+    def concatenate_paths(self, project_name):
+        current_directory = self.get_current_directory()
+        new_directory_path = os.path.join(current_directory, self.root_directory, project_name)
+        return new_directory_path
+
+    
+
